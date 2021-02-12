5960d41a3e435545e76e6be423a9e38fb1cacd89
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-12 23:07:31 +0200

    Add files via upload

---
+++ Task01_Generic_Box_of_String/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task01_Generic_Box_of_String
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            string valueType = Value.GetType().FullName;
+            return $"{valueType}: {Value}";
+        }
+
+    }
+}
---
+++ Task01_Generic_Box_of_String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task01_Generic_Box_of_String
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineNimbers = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= lineNimbers; i++)
+            {
+                Box<string> stringBox = new Box<string>(Console.ReadLine());
+                Console.WriteLine(stringBox);
+            }
+            
+        }
+    }
+}

09f35ea976bc078d3586e25faa50c9e05b630bda
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-12 23:08:06 +0200

    Add files via upload

---
+++ Task02_Generic_Box_of_Integer/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task02_Generic_Box_of_Integer
+{
+    public class Box<T>
+    {
+        //private T value;
+
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType().FullName.ToString()}: {Value}";
+        }
+
+    }
+}
---
+++ Task02_Generic_Box_of_Integer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task02_Generic_Box_of_Integer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int boxLines = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= boxLines; i++)
+            {
+                Box<int> nextBox = new Box<int>();
+                nextBox.Value = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(nextBox);
+            }
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}

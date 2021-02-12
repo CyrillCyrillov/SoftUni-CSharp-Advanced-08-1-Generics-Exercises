c841405713a7a56ab161e1dddec219f62aeddc8b
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-12 23:53:39 +0200

    Add files via upload

---
+++ Task05_Generic_Count_Method_String/GenericElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task05_Generic_Count_Method_String
+{
+    public class GenericElement<T>
+        where T : IComparable
+    {
+
+        public GenericElement(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+        /*
+        public override string ToString()
+        {
+            return $"{Value.GetType().FullName.ToString()}: {Value}";
+        }
+        */
+    }
+}
---
+++ Task05_Generic_Count_Method_String/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task05_Generic_Count_Method_String
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<GenericElement<string>> custumList = new List<GenericElement<string>>();
+
+            int linesNumber = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= linesNumber; i++)
+            {
+                custumList.Add(new GenericElement<string>(Console.ReadLine()));
+            }
+
+            string toComparison = Console.ReadLine();
+
+            Console.WriteLine(CountBiger(custumList, toComparison));
+
+            
+        }
+
+        private static int CountBiger<T>(List<GenericElement<T>> custumList, string toComparison)
+            where T : IComparable
+        {
+            int count = 0;
+            foreach (GenericElement<T> element in custumList)
+            {
+                if(element.Value.CompareTo(toComparison) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
+

e549d688621aa7759df377d007efc8559c88251a
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-12 23:10:50 +0200

    Add files via upload

---
+++ Task04_Generic_Swap_Method_Integer/GenericElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task04_Generic_Swap_Method_Integer
+{
+    public class GenericElement<T>
+    {
+
+        public GenericElement(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType().FullName.ToString()}: {Value}";
+        }
+
+    }
+}
---
+++ Task04_Generic_Swap_Method_Integer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task04_Generic_Swap_Method_Integer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<GenericElement<int>> stringList = new List<GenericElement<int>>();
+            
+            int linesNumber = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= linesNumber ; i++)
+            {
+                stringList.Add(new GenericElement<int>(int.Parse(Console.ReadLine())));
+            }
+
+            int[] indexes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            // indexOwe -> indexes[0];
+            // indexTwo -> indexes[1];
+
+            SwapElements(stringList, indexes[0], indexes[1]);
+
+            foreach (var element in stringList)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        private static void SwapElements<T>(List<GenericElement<T>> customList, int indexOne, int indexTwo)
+        {
+            GenericElement<T> helpVar = customList[indexOne];
+            customList[indexOne] = customList[indexTwo];
+            customList[indexTwo] = helpVar;
+        }
+    }
+}
